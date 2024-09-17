e0d87cbc5f7cc5e5d08687f0a535f21e3b0b8c7b
Comma Device <device@comma.ai>
2024-09-17 16:30:45 -0400

    change to zmq

--- selfdrive/controls/advisory_speed_receiver.py
+++ selfdrive/controls/advisory_speed_receiver.py
@@ -1,28 +1,30 @@
 import cereal.messaging as messaging
 import socket
+import zmq
 
 def main():
     pm = messaging.PubMaster(['advisorySpeed'])
-    
-    # Set up a UDP socket to receive speed data from the tablet
-    udp_socket = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
-    udp_socket.bind(('192.168.2.1', 9090))  # Comma 3X IP and the port number
 
-    print("Listening for UDP packets on port 9090...")
+    context = zmq.Context()
+    socket = context.socket(zmq.REP)
+    socket.bind("tcp://*:5090")
+    print('Listen on 5090 for ZMQ')
 
     while True:
         try:
-            # Receive data from the socket
-            data, addr = udp_socket.recvfrom(1024)
-            data = data.decode('utf-8').strip()
-            if data:
-                advisory_speed = float(data)  # Convert the string to float
-                
+            #  Wait for next request from client
+            message = socket.recv()
+            print("Received request: %s" % message)
+            #  Send reply back to client
+            socket.send(b"ok")
+            if message:
+                advisory_speed = float(message)  # Convert the string to float
+                print("send speed to " + advisory_speed)
                 # Create and send the message using custom.AdvisorySpeed
-                dat = messaging.new_message('advisorySpeed')
-                dat.advisorySpeed.speed = advisory_speed
+                dat = messaging.new_message('customReserved1')
+                dat.customReserved1.advisorySpeed = advisory_speed
                 pm.send('advisorySpeed', dat)
-            
+
         except Exception as e:
             print(f"Error: {e}")
 
